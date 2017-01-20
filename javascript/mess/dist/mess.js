340c99e42934059cf56c4c34dd913c887c7cccfc
tebaly <support@freedomsex.net>
2017-01-20 20:16:51 +0300

    abuse-js

--- javascript/mess/dist/mess.js
+++ javascript/mess/dist/mess.js
@@ -39,23 +39,78 @@ $( document ).ready(function()
 
 
 
+Vue.component('abuse-form', {
+    template: '#abuse-form',
+    props: [
+        'show'
+    ],
+    data: function () {
+        return {
+            isEditorShow:  0,
+            isSuggestShow: 1,
+            message: 'привет!',
+            suggest: [
+                'Предложение оплаты услуг, вирт за деньги, проституция',
+                'Мошенничество, развод на деньги, шантаж, вымогательство',
+                'Фото из интернета, вымышленные данные, обман, фейк',
+                'Оскорбления, хамство, троллинг, грубые сообщения',
+                'Рассылает интим фото, спамит или провоцирует',
+            ],
+            text: '',
+        }
+    },
+    created: function () {
+        console.log("abuse_form Created");
+    },
+    methods: {
+        choiceText: function (event) {
+            this.text = event.target.innerHTML;
+            this.isEditorShow = true;
+            this.isSuggestShow = false;
+        },
+        post: function (event) {
+            simple_hash();
+            this.$http.post('/abuse/send', {
+                id: tid, 
+                abuse: this.text, 
+                captcha: '', 
+                hash: hash 
+            }).then(function(data) {
+                console.log('ok');
+            }, function(data) {
+                console.log(data);
+            });
+        },
+        cancel: function () {
+            this.$emit('cancel');
+            this.isSuggestShow = true;
+        }
+    }
+});
+
 var abuse_list_component = new Vue({
     el: '#abuse-list-component',
     data: {
         abuseStatus:   0,
         isFormShow:    1,
-        isEditorShow:  0,
-        isSuggestShow: 1,
         isListShow:    0,
         isButtonShow:  0,
     },
+    mounted: function () {
+        //console.log(abuse_form.mess());
+    },
     methods: {
         showForm: function (event) {
-            this.isFormShow = !this.isFormShow;
+            if (!this.isFormShow) {
+                this.isFormShow = true;
+            } else {
+                this.hideForm();
+            }
         },
         hideForm: function (event) {
-            this.isFormShow = false;
-            this.isButtonShow = false;
+            this.isFormShow    = false;
+            this.isButtonShow  = false;
+            //this.isSuggestShow = true;
         },
         showList: function (event) {
             this.isListShow = !this.isListShow;
@@ -66,10 +121,6 @@ var abuse_list_component = new Vue({
             } else {
                 this.hideForm();
             }
-        },
-        choiceText: function (event) {
-            this.isEditorShow = true;
-            this.isSuggestShow = false;
         },
     }
 });
