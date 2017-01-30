8367f99fbfd0b5c129f6494c9563e35d65f249c5
tebaly <support@freedomsex.net>
2017-01-30 14:31:12 +0300

    rev-voting-action

--- javascript/admin/src/revs-list.js
+++ javascript/admin/src/revs-list.js
@@ -1,43 +1,50 @@
 
 // -- Список отзывов ---
 var revs_list = {
-     
-    init: function () 
-    {         
-        $('.revs_up').on('click',revs_list.ajax.up); 
-        $('.revs_down').on('click',revs_list.ajax.down); 
-    } , 
-    
-    ajax: {   
-    
-        up: function () 
-        {           
-            var id  = $(this).data('num'); 
+
+    init: function ()
+    {
+        $('.revs_up').on('click',revs_list.ajax.up);
+        $('.revs_down').on('click',revs_list.ajax.down);
+    } ,
+
+    ajax: {
+
+        up: function ()
+        {
+            var id  = $(this).data('num');
             $(this).off('click');
             $.post( '/security/modrevs/', { id: id, mod: 1 } );
-            revs_list.actions.mod(id,1);  
-        } ,  
-    
-        down: function () 
-        {            
-            var id  = $(this).data('num');  
+            revs_list.actions.mod(id,1);
+            revs_list.actions.toggleUp(id);
+        } ,
+
+        down: function ()
+        {
+            var id  = $(this).data('num');
             $(this).off('click');
-            $.post( '/security/modrevs/', { id: id, mod: -1 } ); 
-            revs_list.actions.mod(id,-1);   
-        }      
-          
+            $.post( '/security/modrevs/', { id: id, mod: -1 } );
+            revs_list.actions.mod(id,-1);
+            revs_list.actions.toggleDown(id);
+        }
+
     } ,
- 
+
     actions: {
-    
-        mod: function (id,mod) 
-        {          
+        mod: function (id,mod) {
             var rank = $('#revs_rank_'+id).text() * 1 + mod;
             var vote = $('#revs_vote_'+id).text() * 1 + 1;
-            $('#revs_rank_'+id).text(rank); 
-            $('#revs_vote_'+id).text(vote);       
-        }  
-        
-    } 
-}   
+            $('#revs_rank_'+id).text(rank);
+            $('#revs_vote_'+id).text(vote);
+        },
+        toggleUp: function (id) {
+            $('.revs_up').filter(function () {return $(this).data('num') == id;}).toggleClass('btn-primary btn-success disabled');
+        },
+        toggleDown: function (id) {
+            $('.revs_up').filter(function () {return $(this).data('num') == id;}).toggleClass('btn-primary btn-default');
+            $('.revs_down').filter(function () {return $(this).data('num') == id;}).toggleClass('btn-default btn-danger disabled');
+        },
+
+    }
+}
 
