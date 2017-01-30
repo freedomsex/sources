ce229bc01a48cf00dbc40060450a5111c491735c
tebaly <support@freedomsex.net>
2017-01-30 15:42:28 +0300

    rev-voting-action-add

--- javascript/admin/dist/admin.js
+++ javascript/admin/dist/admin.js
@@ -323,6 +323,7 @@ var revs_list = {
     {
         $('.revs_up').on('click',revs_list.ajax.up);
         $('.revs_down').on('click',revs_list.ajax.down);
+        $('#review-all-bad').on('click',revs_list.actions.allBad);
     } ,
 
     ajax: {
@@ -353,6 +354,14 @@ var revs_list = {
             var vote = $('#revs_vote_'+id).text() * 1 + 1;
             $('#revs_rank_'+id).text(rank);
             $('#revs_vote_'+id).text(vote);
+            $('#review-item-'+id).data('voted', 1);
+        },
+        allBad: function () {
+            $('.revs_down').filter(function () {
+                var id = $(this).data('num');
+                var voted = $('#review-item-'+id).data('voted');
+                return voted ? 0 : 1;
+            }).click();
         },
         toggleUp: function (id) {
             $('.revs_up').filter(function () {return $(this).data('num') == id;}).toggleClass('btn-primary btn-success disabled');
