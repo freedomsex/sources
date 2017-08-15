38c436b9471e2bcfe2be1b1bc81153d10828dc6d
tebaly <support@freedomsex.net>
2017-08-15 20:31:07 +0300

    clean-old

--- javascript/core/src/2-init.js
+++ javascript/core/src/2-init.js
@@ -79,38 +79,8 @@ new Vue({
     router: settingsRouter
 });
 
-
-
-
-
-
-
-
-$(document).ready(function()
-{
-    //userinfo.init();
-    slider.init();
-    //giper_chat.init();
-    notepad.init();
-
-    mailsett.init();
-    report.init();
+ 
+$(document).ready(function() { 
     navigate.init();
-
-    name_suggest.init();
-    city_suggest.init();
-
-    option_static.init();
-    option_sex.init();
-    //option_email.init();
-    profile_alert.init();
-    profile_option.init();
-
-    //user_tag.init();
-    //desire_clip.init();
-
-    //result_list.init();
-    //visited.init();
-
 });
 
--- javascript/core/src/user-menu.js
+++
@@ -1,7 +0,0 @@
-
-var user_menu = { init: function () {},
-    ajax: {},
-    action: {sets: {search: function () { },contact: function () {}}},
-    option: {act: { }, se: function () {}}
-}
-
