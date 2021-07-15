1e8b58c9992320bf3abf39b0c6e01f572f306a27
tebaly <support@freedomsex.net>
2021-07-15 14:31:12 +0300

    fix: mess fromId

--- src/components/activities/messages/MessageItem.vue
+++ src/components/activities/messages/MessageItem.vue
@@ -50,7 +50,7 @@ export default {
       return this.showOption || this.fixOption ? 1 : 0;
     },
     sent() {
-      return !this.uid || this.uid == this.item.sender ? 1 : 0;
+      return !this.uid || this.uid == this.item.fromId ? 1 : 0;
     },
     read() {
       return this.item.readed != 0;
@@ -76,7 +76,7 @@ export default {
     },
     previous() {
       const p = prev;
-      prev = this.item.sender;
+      prev = this.item.fromId;
       return !!(!p || p == prev);
     },
     acceptRemove() {
@@ -96,13 +96,17 @@ export default {
     bun() {
       const data = {
         id: this.item.id,
-        tid: this.item.sender,
+        tid: this.item.fromId,
       };
-      this.$api.res('mess/bun', 'raw').post(data).then(() => {
-        this.$emit('remove', this.index);
-      }).catch(() => {
-        console.log('error');
-      });
+      this.$api
+        .res('mess/bun', 'raw')
+        .post(data)
+        .then(() => {
+          this.$emit('remove', this.index);
+        })
+        .catch(() => {
+          console.log('error');
+        });
     },
     cancel() {
       this.confirm.remove = false;
@@ -112,9 +116,12 @@ export default {
       const data = {
         id: this.item.id,
       };
-      this.$api.res('mess/delete', 'raw').post(data).then(() => {
-        // this.$emit('remove', this.index);
-      })
+      this.$api
+        .res('mess/delete', 'raw')
+        .post(data)
+        .then(() => {
+          // this.$emit('remove', this.index);
+        })
         .catch((error) => {
           console.log(error);
         });
@@ -132,11 +139,9 @@ export default {
     play() {
       const config = {
         headers: {Authorization: `Bearer ${this.$store.state.token.access}`},
-        params: {tid: this.item.sender},
+        params: {tid: this.item.fromId},
       };
-      const url = `${CONFIG.API_PHOTO}/api/v1/users/${this.uid}/sends/${
-        this.alias
-      }.jpg`;
+      const url = `${CONFIG.API_PHOTO}/api/v1/users/${this.uid}/sends/${this.alias}.jpg`;
       axios
         .get(url, config)
         .then((response) => {
@@ -188,66 +193,66 @@ export default {
     <div class="message-item__spliter" v-show="!previous"></div>
     <div :class="[sent ? 'message-item__sent' : 'message-item__received']">
       <div class="message-item__balloon" @mouseover="showOption = true" @click="fix">
-        <div v-if="image" class="message-image" :style="{ backgroundImage: 'url('+image+')' }">
+        <div v-if="image" class="message-image" :style="{backgroundImage: 'url(' + image + ')'}">
           <div class="message-image__play" @click="play">
-            <img src="~static/img/play.png">
+            <img src="~static/img/play.png" />
           </div>
           <span class="message-image__info">
             <span class="message-image__status" v-show="!read"></span>
-            <span class="message-image__time">{{time}}</span>
+            <span class="message-image__time">{{ time }}</span>
           </span>
         </div>
         <span v-else>
-          <CensoredText :text="item.message" :bypass="sent"/>
+          <CensoredText :text="item.message" :bypass="sent" />
           <span class="message-info">
             <span class="message-info__status" v-show="!read"></span>
-            <span class="message-info__time">{{time}}</span>
+            <span class="message-info__time">{{ time }}</span>
           </span>
-
         </span>
       </div>
-      <span class="message-item__remove" :class="attention ? 'message-item__bunn-alert' : 'message-item__remove'"
-       v-show="option"
-       @click="confirmRemove(false)">
+      <span
+        class="message-item__remove"
+        :class="attention ? 'message-item__bunn-alert' : 'message-item__remove'"
+        v-show="option"
+        @click="confirmRemove(false)"
+      >
         <span aria-hidden="true" class="glyphicon glyphicon-remove"></span>
       </span>
     </div>
 
     <span v-if="confirm.remove">
-      <ConfirmDialog v-if="sent"
+      <ConfirmDialog
+        v-if="sent"
         yesText="Удалить сообщение"
         @confirm="remove()"
-        @close="confirm.remove = false">
+        @close="confirm.remove = false"
+      >
         <span slot="title">Автоочистка</span>
-        Все сообщения из переписки удаляются автоматически
-        в течении недели. Удалять сообщение
-        на которые уже поступила жалоба бессмысленно.
+        Все сообщения из переписки удаляются автоматически в течении недели. Удалять сообщение на
+        которые уже поступила жалоба бессмысленно.
       </ConfirmDialog>
 
-      <ConfirmDialog v-else
+      <ConfirmDialog
+        v-else
         yesText="Наказать и удалить"
         noText="Удалить"
         @confirm="bun()"
         @cancel="remove()"
-        @close="confirm.remove = false">
+        @close="confirm.remove = false"
+      >
         <span slot="title">Очень важно</span>
-        Мы никогда не узнаем о нарушениях, если удалить без наказания.
-        Нажмите "Наказать и удалить" у всех сообщений
-        которые вызывают негатив.
+        Мы никогда не узнаем о нарушениях, если удалить без наказания. Нажмите "Наказать и удалить"
+        у всех сообщений которые вызывают негатив.
       </ConfirmDialog>
     </span>
 
-    <InfoDialog v-if="confirm.accept"
-      @close="confirm.accept = false"
-      @confirm="accept()">
+    <InfoDialog v-if="confirm.accept" @close="confirm.accept = false" @confirm="accept()">
       <span slot="title">Удаляем отовсюду</span>
-      Сообщение пропадет как из вашей истории переписки,
-      так и из переписки вашего собеседника.
+      Сообщение пропадет как из вашей истории переписки, так и из переписки вашего собеседника.
       Сообщения удаляются без возможности восстановить.
     </InfoDialog>
 
-
-    <PhotoSend v-if="photo" :photo="photo" @close="photo = false"/>
+    <PhotoSend v-if="photo" :photo="photo" @close="photo = false" />
     <Toast v-if="photoNotFound" @close="photoNotFound = false">Фото не найдено</Toast>
   </div>
 </template>
@@ -315,11 +320,7 @@ export default {
   left: 0;
   padding: 20px 5px 5px 5px;
   text-align: right;
-  background: linear-gradient(
-    to bottom right,
-    transparent 70%,
-    rgba(0, 0, 0, 0.3)
-  );
+  background: linear-gradient(to bottom right, transparent 70%, rgba(0, 0, 0, 0.3));
 }
 
 .message-image__time {
